c935f5d6d267d10d72938c6b766ffadf7bb51d43
Sudip <sudip@sudips-macbook-pro.local>
2018-12-06 11:34:46 +0530

    Resume

---
+++ Yo! Chat/View/GradientView.swift
@@ -0,0 +1,15 @@
+//
+//  GradientView.swift
+//  Yo! Chat
+//
+//  Created by Sudip on 12/4/18.
+//  Copyright © 2018 Sudeepasa. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    
+
+}
